81b988da9510aa2fdcc865398d8c09a7748fb2a6
Jens Lövgren <jens.lovgren@outlook.com>
2019-11-20 15:29:19 +0100

    Added CatBreeds

---
+++ CSharpGIT/CatBreeds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGIT
+{
+    enum CatBreeds
+    {
+        AmericanCurl,
+        Aegean,
+        Balinese,
+        Bambino,
+        Burmese,
+        Chartreux,
+        MaineCoon,
+        Manx,
+        Ragdoll,
+    }
+}
--- CSharpGIT/Program.cs
+++ CSharpGIT/Program.cs
@@ -6,7 +6,6 @@ namespace CSharpGIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
