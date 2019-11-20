496f57f8117be2c46075bc6413d893b26e994cf3
Pontus Nelander <pontus.nelander@live.se>
2019-11-20 15:34:31 +0100

    Added a utils class with a random number generator

--- CSharpGIT/Program.cs
+++ CSharpGIT/Program.cs
@@ -6,7 +6,9 @@ namespace CSharpGIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+
         }
     }
 }
---
+++ CSharpGIT/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGIT
+{
+    
+    static class Utils
+    {
+        static Random rng = new Random();
+
+        public static int RandomNumberGenerator(int a, int b) => rng.Next(a, b + 1);
+        
+           
+        
+    }
+}
